33d77cdf2cd7b757efcadec4a0b309e0c917500b
Marek Defeciński <marekdef@gmail.com>
2011-12-23 20:37:28 +0100

    Created project

---
+++ src/net/retsat1/starlab/inpost/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package net.retsat1.starlab.inpost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
