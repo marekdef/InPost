e538c5447bf261263fef1fc5b0cee13656ece60d
Marek Defeciński <marekdef@gmail.com>
2012-02-24 20:49:23 +0100

    Created project

---
+++ src/pl/lodz/atp/inpost/InPostTracking.java
@@ -0,0 +1,15 @@
+package pl.lodz.atp.inpost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InPostTracking extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
