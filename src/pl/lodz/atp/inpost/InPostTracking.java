b789f4cb035b3b3901a83c17c7f1f8e901389b5c
Marek Defeciński <marekdef@gmail.com>
2012-02-24 22:26:23 +0100

    Created android code for views and listeners

--- src/pl/lodz/atp/inpost/InPostTracking.java
+++ src/pl/lodz/atp/inpost/InPostTracking.java
@@ -2,14 +2,52 @@ package pl.lodz.atp.inpost;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
-public class InPostTracking extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class InPostTracking extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button buttonClear = (Button) findViewById(R.id.buttonClear);
+		Button buttonFind = (Button) findViewById(R.id.buttonFind);
+		Button buttonScan = (Button) findViewById(R.id.buttonScan);
+		EditText editTextTrackingNumber = (EditText) findViewById(R.id.editTextTrackingNumber);
+		WebView webView = (WebView) findViewById(R.id.webViewStatus);
+		ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+		buttonFind.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		buttonClear.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		buttonScan.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
 }
