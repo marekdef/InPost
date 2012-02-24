e714e9f91a44b1d0466e33020e5629fda56833ec
Marek Defeciński <marekdef@gmail.com>
2012-02-25 00:32:27 +0100

    Added find button feature

--- src/pl/lodz/atp/inpost/InPostTracking.java
+++ src/pl/lodz/atp/inpost/InPostTracking.java
@@ -2,6 +2,7 @@ package pl.lodz.atp.inpost;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
@@ -10,25 +11,43 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 
 public class InPostTracking extends Activity {
+	private HttpParser parser = new HttpParser();
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		Button buttonClear = (Button) findViewById(R.id.buttonClear);
-		Button buttonFind = (Button) findViewById(R.id.buttonFind);
-		Button buttonScan = (Button) findViewById(R.id.buttonScan);
-		EditText editTextTrackingNumber = (EditText) findViewById(R.id.editTextTrackingNumber);
-		WebView webView = (WebView) findViewById(R.id.webViewStatus);
-		ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+		final Button buttonClear = (Button) findViewById(R.id.buttonClear);
+		final Button buttonFind = (Button) findViewById(R.id.buttonFind);
+		final Button buttonScan = (Button) findViewById(R.id.buttonScan);
+		final EditText editTextTrackingNumber = (EditText) findViewById(R.id.editTextTrackingNumber);
+		final WebView webView = (WebView) findViewById(R.id.webViewStatus);
+		final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
 		buttonFind.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
+				final String trackingNumber = editTextTrackingNumber.getText().toString();
+				progressBar.setVisibility(View.VISIBLE);
+				webView.setVisibility(View.GONE);
+				
+				Handler handler = new Handler();
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							String parsedInformation = parser.execute(trackingNumber);
+							webView.loadDataWithBaseURL(null, parsedInformation, "text/html", "utf-8", null);
+						}
+						finally {
+							progressBar.setVisibility(View.GONE);
+							webView.setVisibility(View.VISIBLE);
+						}
+					}
+				});
 			}
 		});
 
@@ -36,8 +55,6 @@ public class InPostTracking extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
 			}
 		});
 
@@ -45,8 +62,6 @@ public class InPostTracking extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
 			}
 		});
 	}
