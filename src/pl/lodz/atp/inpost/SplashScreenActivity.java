1340380429c632811a074bd523e891cd3bd45e61
Marek Defeciński <marekdef@gmail.com>
2012-03-20 22:48:02 +0100

    Added transition from SplashScreenActivity to InPostTrackingActivity via Intent

--- src/pl/lodz/atp/inpost/SplashScreenActivity.java
+++ src/pl/lodz/atp/inpost/SplashScreenActivity.java
@@ -1,7 +1,10 @@
 package pl.lodz.atp.inpost;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SplashScreenActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,14 @@ public class SplashScreenActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				startActivity(new Intent(SplashScreenActivity.this, InPostTracking.class));
+				
+			}
+		}, 2000);
     }
 }
