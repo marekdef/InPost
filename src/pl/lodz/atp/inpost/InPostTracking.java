84ff15a40c56779392b3e4d97d28687b06a6fe42
Marek Defeciński <marekdef@gmail.com>
2012-03-20 22:46:57 +0100

    Added new activity that will display main screen of the application to the AndroidManifest.xml

---
+++ src/pl/lodz/atp/inpost/InPostTracking.java
@@ -0,0 +1,16 @@
+package pl.lodz.atp.inpost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InPostTracking extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
