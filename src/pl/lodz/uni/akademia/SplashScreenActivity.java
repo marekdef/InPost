40fce3293e594672b384e5e443565eae2d99ee88
Marek Defeciński <marekdef@gmail.com>
2012-04-02 22:31:16 +0200

    Added new activity.\nUsing startActivity with Intent inside runnable sent by means of Handler.

--- src/pl/lodz/uni/akademia/SplashScreenActivity.java
+++ src/pl/lodz/uni/akademia/SplashScreenActivity.java
@@ -1,13 +1,24 @@
 package pl.lodz.uni.akademia;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends Activity {
-    /** Called when the activity is first created. */
+    private static final long SPLASH_DELAY = 2000;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				startActivity(new Intent(getApplicationContext(), InPostTrackerActivity.class));
+			}
+		}, SPLASH_DELAY);
     }
 }
