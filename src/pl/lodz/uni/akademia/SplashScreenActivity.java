694c4354f7875887a280b0b59b8a7ed65822a1b1
Marek Defeciński <marekdef@gmail.com>
2012-06-10 23:38:02 +0200

    Added transtion on click. Removing splash from history. Preventing InPostTracker activity to be placed twice on the stack

--- src/pl/lodz/uni/akademia/SplashScreenActivity.java
+++ src/pl/lodz/uni/akademia/SplashScreenActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class SplashScreenActivity extends Activity {
-    private static final long SPLASH_DELAY = 2000;
+    private static final long SPLASH_DELAY = 3000;
 
 	/** Called when the activity is first created. */
     @Override
@@ -14,11 +16,33 @@ public class SplashScreenActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        new Handler().postDelayed(new Runnable() {
+        View splashImage = findViewById(R.id.imageViewSplash);
+        splashImage.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				proceedToInPostTracker();
+			}
+		});
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	
+    	new Handler().postDelayed(new Runnable() {
+			
 			@Override
 			public void run() {
-				startActivity(new Intent(getApplicationContext(), InPostTrackerActivity.class));
+				proceedToInPostTracker();
+				
 			}
 		}, SPLASH_DELAY);
+    }
+    
+    private void proceedToInPostTracker() {
+    	Intent intent = new Intent(getApplicationContext(), InPostTrackerActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		startActivity(intent);
     }
 }
