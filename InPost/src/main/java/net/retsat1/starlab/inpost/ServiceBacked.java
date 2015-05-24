10db05d0ecf0c656569251659f3fcc3094c08d52
Marek Defeciński <Marek.Defecinski@tomtom.com>
2015-05-24 23:58:28 +0200

    Service

--- InPost/src/main/java/net/retsat1/starlab/inpost/ServiceBacked.java
+++ InPost/src/main/java/net/retsat1/starlab/inpost/ServiceBacked.java
@@ -1,70 +1,122 @@
 package net.retsat1.starlab.inpost;
 
+import net.retsat1.starlab.inpost.TrackingService.*;
+import net.retsat1.starlab.inpost.exceptions.TimeoutException;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v4.content.LocalBroadcastManager;
 
 import rx.Observable;
-import rx.Observer;
-import rx.subjects.ReplaySubject;
+import rx.subjects.PublishSubject;
 import rx.subjects.Subject;
+
+import static net.retsat1.starlab.inpost.QueryService.ACTION_FAILURE;
+import static net.retsat1.starlab.inpost.QueryService.ACTION_RESULT;
+import static net.retsat1.starlab.inpost.QueryService.ACTION_TIMEOUT;
+import static net.retsat1.starlab.inpost.QueryService.RESULT;
 
 /**
  * Created by marekdef on 20.04.15.
  */
-public class ServiceBacked implements TrackingService, Observer<String> {
+public class ServiceBacked implements TrackingService {
+
+    public static final String TRACKING_RESULT = "resultsStream";
+
+    public static final String TRACKING_NUMBER = "tracking_result";
+
+    public static final String SHARED_PREFERENCE_NAME = "trackings";
+
+    private static final long TIMEOUT = 10_000;
+
     private final Context context;
-    private Subject<Result, Result > result = ReplaySubject.<Result>create();
+
+    private final SharedPreferences sharedPreferences;
+
+    private final Handler handler;
+
+    private Subject resultsStream = PublishSubject.<Result>create();
+
+    private Subject errorsStream = PublishSubject.<Error>create();
 
     public ServiceBacked(Context applicationContext) {
         this.context = applicationContext.getApplicationContext();
+        this.sharedPreferences = applicationContext.getSharedPreferences(SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+        handler = new Handler();
+        
+        loadResult(this.sharedPreferences);
     }
 
-    public void checkTracking(final String numer_przesylki) {
+    private void loadResult(SharedPreferences sharedPreferences) {
+        final String trackingNumber = sharedPreferences.getString(TRACKING_NUMBER, null);
+        final String result = sharedPreferences.getString(TRACKING_RESULT, null);
+
+        if (trackingNumber != null && result != null) {
+            this.resultsStream.onNext(new TrackingService.Result(trackingNumber, result));
+        }
+    }
+
+    public void sendQuery(final String trackingNumber) {
+
+
 
         IntentFilter filter = new IntentFilter();
-        filter.addAction(QueryService.ACTION_RESULT);
-        filter.addAction(QueryService.ACTION_FAILURE);
-
+        filter.addAction(ACTION_RESULT);
+        filter.addAction(ACTION_FAILURE);
+        filter.addAction(ACTION_TIMEOUT);
 
         final BroadcastReceiver receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                if(QueryService.ACTION_RESULT.equals(intent.getAction())) {
-                    result.onNext(new TrackingService.Result(numer_przesylki, intent.getStringExtra(QueryService.RESULT)));
-                }
-                else if(QueryService.ACTION_FAILURE.equals(intent.getAction())) {
-                    result.onError((Exception) intent.getSerializableExtra(QueryService.RESULT));
+                final String action = intent.getAction();
+                if (ACTION_RESULT.equals(action) || ACTION_FAILURE.equals(action)) {
+                    final TrackingService.Result result = (TrackingService.Result) intent.getSerializableExtra
+                            (RESULT);
+
+                    resultsStream.onNext(result);
+                    saveResult(result);
+                } else if (ACTION_TIMEOUT.equals(action) || ACTION_FAILURE.equals(action)) {
+                    final Error error = (Error) intent.getSerializableExtra
+                            (RESULT);
+                    errorsStream.onNext(error);
+                    return;//skip unregistering for timeout we still might get an answer
                 }
                 LocalBroadcastManager.getInstance(context).unregisterReceiver(this);
             }
         };
 
         LocalBroadcastManager.getInstance(context).registerReceiver(receiver, filter);
-        QueryService.startActionFoo(context, numer_przesylki);
+        
+        
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent timeoutIntent = new Intent(ACTION_TIMEOUT);
+                timeoutIntent.putExtra(RESULT, new TrackingService.Error(trackingNumber, new TimeoutException()));
+                LocalBroadcastManager.getInstance(ServiceBacked.this.context).sendBroadcast(timeoutIntent);
+            }
+        }, TIMEOUT);
+        QueryService.startActionFoo(context, trackingNumber);
+    }
 
-
+    private void saveResult(TrackingService.Result result) {
+        ServiceBacked.this.sharedPreferences.edit()
+                                            .putString(TRACKING_RESULT, result.result)
+                                            .putString(TRACKING_NUMBER, result.number)
+                                            .apply();
     }
 
     @Override
-    public Observable<Result> getObservable() {
-        return result;
+    public Observable<Result> getResultStream() {
+        return resultsStream;
     }
 
     @Override
-    public void onCompleted() {
-
-    }
-
-    @Override
-    public void onError(Throwable e) {
-
-    }
-
-    @Override
-    public void onNext(String numer) {
-        checkTracking(numer);
+    public Observable<Error> getErrorStream() {
+        return errorsStream;
     }
 }
